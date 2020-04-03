9df17d2801446b5689d20e1648d61d50907b9c00
Maathavan <maathumaathavan@gmail.com>
2020-04-03 16:38:14 +0530

    Refactoring

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+
+        const firebaseConfig = {
+            apiKey: "AIzaSyCHAsGid0iA0FUpL0F-HGanJ3jvzfD8uqs",
+            authDomain: "varaipadavi.firebaseapp.com",
+            databaseURL: "https://varaipadavi.firebaseio.com",
+            projectId: "varaipadavi",
+            storageBucket: "varaipadavi.appspot.com",
+            messagingSenderId: "120572762230",
+            appId: "1:120572762230:web:b2c04402f971a05b0fa757",
+            measurementId: "G-2ZNXL3S5RR"
+        };
+
+        firebase.initializeApp(firebaseConfig);
